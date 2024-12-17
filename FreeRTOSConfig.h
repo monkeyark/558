c54ba8f7c7ec717025bf998d3b3b52a1267bbffd
monkeyark <monkeyark@gmail.com>
2024-12-17 17:38:01 -0600

    API configRUN_MULTIPLE_PRIORITIES

--- FreeRTOSConfig.h
+++ FreeRTOSConfig.h
@@ -19,24 +19,25 @@
  * See http://www.freertos.org/a00110.html.
  *----------------------------------------------------------*/
 
-	#define configUSE_PREEMPTION				1
-	#define configUSE_IDLE_HOOK					1
-	#define configUSE_TICK_HOOK					0
-	#define configTICK_RATE_HZ					( ( portTickType ) 1000 )
-	#define configMINIMAL_STACK_SIZE			( ( unsigned portSHORT ) 64 ) /* This can be made smaller if required. */
-	#define configTOTAL_HEAP_SIZE				( ( size_t ) ( 64 * 1024 ) )
-	#define configMAX_TASK_NAME_LEN				( 16 )
-	#define configUSE_TRACE_FACILITY			0
-	#define configUSE_16_BIT_TICKS				0
-	#define configIDLE_SHOULD_YIELD				1
-	#define configUSE_MUTEXES					1
-	#define configCHECK_FOR_STACK_OVERFLOW		0 /* Do not use this option on the PC port. */
-	#define configUSE_RECURSIVE_MUTEXES			1
-	#define configQUEUE_REGISTRY_SIZE			20
-	#define configUSE_MALLOC_FAILED_HOOK		0
-	#define configUSE_APPLICATION_TASK_TAG		1
-	#define configUSE_COUNTING_SEMAPHORES		1
-	#define configUSE_ALTERNATIVE_API			0
+#define configUSE_PREEMPTION					1
+#define configUSE_IDLE_HOOK						1
+#define configUSE_TICK_HOOK						0
+#define configTICK_RATE_HZ						( ( portTickType ) 1000 )
+#define configMINIMAL_STACK_SIZE				( ( unsigned portSHORT ) 64 ) /* This can be made smaller if required. */
+#define configTOTAL_HEAP_SIZE					( ( size_t ) ( 64 * 1024 ) )
+#define configMAX_TASK_NAME_LEN					( 16 )
+#define configUSE_TRACE_FACILITY				0
+#define configUSE_16_BIT_TICKS					0
+#define configIDLE_SHOULD_YIELD					1
+#define configUSE_MUTEXES						1
+#define configCHECK_FOR_STACK_OVERFLOW			0 /* Do not use this option on the PC port. */
+#define configUSE_RECURSIVE_MUTEXES				1
+#define configQUEUE_REGISTRY_SIZE				20
+#define configUSE_MALLOC_FAILED_HOOK			0
+#define configUSE_APPLICATION_TASK_TAG			1
+#define configUSE_COUNTING_SEMAPHORES			1
+#define configUSE_ALTERNATIVE_API				0
+#define configRUN_MULTIPLE_PRIORITIES			1
 	//#define configMAX_SYSCALL_INTERRUPT_PRIORITY	1
 
 #define configUSE_QUEUE_SETS					1
@@ -71,7 +72,6 @@ functions anyway. */
 #define INCLUDE_xTimerPendFunctionCall			1
 #define INCLUDE_xTaskAbortDelay					1
 #define INCLUDE_xTaskGetHandle					1
-#define configRUN_MULTIPLE_PRIORITIES			1
 
 /* It is a good idea to define configASSERT() while developing.  configASSERT()
 uses the same semantics as the standard C assert() macro. */
